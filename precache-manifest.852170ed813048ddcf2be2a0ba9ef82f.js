0154fab2c5ab3d91c56af621be8e1962cce9ab5e
Ashish haldar <105638023+webdev2190@users.noreply.github.com>
2022-08-30 13:19:14 +0530

    Updates

---
+++ precache-manifest.852170ed813048ddcf2be2a0ba9ef82f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f5c86b0b8eb0f42216b",
+    "url": "/React-TODO-LIST/static/css/main.c90a70e4.chunk.css"
+  },
+  {
+    "revision": "3f5c86b0b8eb0f42216b",
+    "url": "/React-TODO-LIST/static/js/main.3f5c86b0.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/React-TODO-LIST/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "044c02edd923e5fbfcc9",
+    "url": "/React-TODO-LIST/static/css/2.3be0edfe.chunk.css"
+  },
+  {
+    "revision": "044c02edd923e5fbfcc9",
+    "url": "/React-TODO-LIST/static/js/2.044c02ed.chunk.js"
+  },
+  {
+    "revision": "0824ad4eb5c11925c1d3",
+    "url": "/React-TODO-LIST/static/js/runtime~main.0824ad4e.js"
+  },
+  {
+    "revision": "4656324af40b2df35749295186581b1f",
+    "url": "/React-TODO-LIST/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactplan/precache-manifest.04567d5b21c522ec335519538e85b3af.js"
+  "/React-TODO-LIST/precache-manifest.852170ed813048ddcf2be2a0ba9ef82f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactplan/index.html", {
+workbox.routing.registerNavigationRoute("/React-TODO-LIST/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
